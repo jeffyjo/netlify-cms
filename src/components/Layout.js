6909c7f3f4b169cd397feb3dee67fe12c6c48fbc
Jeff Jørgensen <jyj@pentia.dk>
2019-01-28 13:28:34 +0100

    Update style

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from "gatsby"
 
 import Navbar from '../components/Navbar'
-import './all.sass'
+import '../styles/style.scss'
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
