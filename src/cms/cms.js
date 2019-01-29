dd1c8e39c83742f3ab1df6148fe9b0c222ce5571
Jeff Jørgensen <jyj@pentia.dk>
2019-01-29 12:32:35 +0100

    Add test page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import TestPagePreview from './preview-templates/TestPagePreview';
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('test', TestPagePreview)
