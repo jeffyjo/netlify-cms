a80ca38f2ddb24b14ac36efa445c56fed2798b99
Jeff Jørgensen <jyj@pentia.dk>
2019-02-08 10:52:19 +0100

    Add search component

--- src/components/01-atoms/TextField/TextField.js
+++ src/components/01-atoms/TextField/TextField.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import propTypes from 'prop-types'
 
-const TextField = ({type, componentClass}) => {
+const TextField = ({type, componentClass, action, value}) => {
 
   return (
     <input 
       className={"a-text-field " + componentClass ? componentClass : ''}
       type={type} 
+      onChange={action}
+      value={value}
     />
   )
 }
 
 TextField.propTypes = {
   type: propTypes.string.isRequired,
-  componentClass: propTypes.string
+  componentClass: propTypes.string,
+  action: propTypes.func,
+  value: propTypes.string
 }
 
 export default TextField
--- src/components/02-molecules/Search/Search.js
+++ src/components/02-molecules/Search/Search.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react'
+import { navigate } from 'gatsby'
+
+import { 
+  fromObjectToParams,
+  fromParamsToObject 
+} from '../../util/Util'
 
 import TextField from '../../01-atoms/TextField/TextField'
 import './Search.scss'
 
 class Search extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchValue: ''
+    }
+
+    this.setSearchParams = this.setSearchParams.bind(this)
+    this.getSearchParams = this.getSearchParams.bind(this)
+    this.onSubmitSearch = this.onSubmitSearch.bind(this)
+  }
+
+  getSearchParams() {
+    let queryObject = fromParamsToObject()
+    this.setState({
+      searchValue: queryObject.search
+    })
+  }
+
+  setSearchParams(e, path = '') {
+    let value = e.target.value
+
+    if(value.length <= 0) {
+      navigate('/')
+      return
+    }
+    
+    let data = {
+      key: 'search',
+      value: value
+    }
+    
+    let query = fromObjectToParams(data)
+    navigate(`${path}${query}`)
+  }
+
+  onSubmitSearch(e) {
+    e.preventDefault()
+    navigate('/search-results')
+  }
+
+  componentDidMount() {
+    this.getSearchParams()
+  }
+
   render() {
       return (
         <div className="m-search">
-        <TextField type="search" componentClass="m-search__input" />
+          <form onSubmit={(e) => this.onSubmitSearch(e)}>
+            <TextField type="search" componentClass="m-search__input" value={this.state.searchValue} action={(e) => this.setSearchParams(e)} />
+          </form>
       </div>
     )
   }
