435c2f42e741d3dc8c4bda8a3fabde3daea57ba6
Jeff Jørgensen <jyj@pentia.dk>
2019-02-11 11:44:19 +0100

    Update taglist

--- src/components/02-molecules/NavList/NavList.js
+++ src/components/02-molecules/NavList/NavList.js
@@ -5,11 +5,11 @@ import './NavList.scss'
 
 const NavList = () => {
   return (
-    <div className="m-navbar">
-      <Link className="m-navbar__item" to="/">
+    <div className="m-navlist">
+      <Link className="m-navlist__item" to="/">
         Articles
       </Link>
-      <Link className="m-navbar__item" to="/tags">
+      <Link className="m-navlist__item" to="/tags">
         Tags
       </Link>
     </div>
