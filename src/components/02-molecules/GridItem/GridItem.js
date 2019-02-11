456c7bd4b1500f5b07bf3de46d2c9c8394eb8533
Jeff Jørgensen <jyj@pentia.dk>
2019-02-11 12:03:14 +0100

    Add link to search-results

--- src/components/02-molecules/GridItem/GridItem.js
+++ src/components/02-molecules/GridItem/GridItem.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import './GridItem.scss'
 
 let GridItem = ({item}) => {
-  const { title, date } = item.node.frontmatter 
+  const { frontmatter : { title, date }, fields: { slug: url } } = item.node
+
   return (
-    <div className="m-grid-item">
+    <Link to={url} className="m-grid-item">
       <h3 className="m-grid-item__title">{title}</h3>
-      <time>{date}</time>
-    </div>
+      <time className="m-grid-item__date">{date}</time>
+    </Link>
   )
 }
 
