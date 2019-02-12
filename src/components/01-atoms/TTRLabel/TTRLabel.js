e079bb49a31906d56d1590798776e81676b4c0e5
Jeff Jørgensen <jyj@pentia.dk>
2019-02-12 09:45:28 +0100

    Add TTR label

---
+++ src/components/01-atoms/TTRLabel/TTRLabel.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './TTRLabel.scss'
+
+const TTRLabel = ({ TTRText }) => {
+  return (
+    <div className="a-ttr-label">Time to Read: {TTRText} min.</div>
+  )
+}
+
+TTRLabel.proptypes = {
+  TTRText: PropTypes.string.isRequired
+}
+
+export default TTRLabel
