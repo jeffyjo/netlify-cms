e39738198877b2ac5df334493999576cda8101ca
Jeff Jørgensen <jyj@pentia.dk>
2019-03-22 11:14:17 +0100

    Update articleContainer

--- src/container/ArticleContainer/ArticleContainer.js
+++ src/container/ArticleContainer/ArticleContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-import ArticleItem from '../components/02-molecules/ArticleItem/ArticleItem'
+import ArticleItem from '../../components/02-molecules/ArticleItem/ArticleItem'
+
+import './ArticleContainer.scss'
 
 const ArticleContainer = ({ articles }) => {
   return (
-    <div>
-      <ul>
+    <div className="c-article-container">
+      <ul className="c-article-container__list">
         {articles
           ? articles.map((article, i) => (
               <ArticleItem article={article} key={i} />
